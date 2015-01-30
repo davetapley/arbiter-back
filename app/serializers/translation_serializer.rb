f3502ab3200225588446be898ff2a42c52e3b8fa
Dave Tapley <dukedave@gmail.com>
2015-01-30 11:12:52 -0700

    Front end now handles active calculation independently

--- app/serializers/translation_serializer.rb
+++ app/serializers/translation_serializer.rb
@@ -1,11 +1,7 @@
 class TranslationSerializer < ActiveModel::Serializer
-  attributes :priority, :rule, :target, :active
+  attributes :priority, :rule, :target
 
   def rule
     object.rule_config.merge({ :$type => object.rule_type })
-  end
-
-  def active
-    object.rule.active?
   end
 end

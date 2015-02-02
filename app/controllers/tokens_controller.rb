5966327c3350c4e28bb9316e338728142d67d1c0
Dave Tapley <dukedave@gmail.com>
2015-02-02 16:35:57 -0700

    Scope tokens to their user

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -2,19 +2,19 @@ class TokensController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: Token.all
+    render json: current_user.tokens
   end
 
   def show
-    render json: Token.find(params[:id])
+    render json: current_user.tokens.find(params[:id])
   end
 
   def update
-    params[:token][:translations].each_with_index do |translation_params, index|
-      translation_params = translation_params.to_hash
+    token = current_user.tokens.find(params[:id])
 
-      translation = Translation.find_or_initialize_by token: params[:id], priority: index
-      translation.update_attributes! translation_params.merge(priority: index)
+    params[:token][:translations].each_with_index do |translation_params, index|
+      translation = token.translations.find_or_initialize_by priority: index
+      translation.update_attributes! translation_params.to_hash.merge(priority: index)
     end
   end
 

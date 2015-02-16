2cce622ddae13f1ce82014c549c97fcee6964762
Dave Tapley <dukedave@gmail.com>
2015-02-16 14:51:46 -0700

    Front end now treats IDs as comma separated strings

--- app/serializers/token_serializer.rb
+++ app/serializers/token_serializer.rb
@@ -2,4 +2,8 @@ class TokenSerializer < ActiveModel::Serializer
   has_many :translations, serializer: TranslationSerializer
 
   attributes :id, :translations
+
+  def id
+    "#{ object.domain_id },#{ object.path_id }"
+  end
 end

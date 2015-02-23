cdb33bf7b9ac38090ada95c3a4e0de2306addb58
Dave Tapley <dukedave@gmail.com>
2015-02-23 11:31:44 -0700

    Don't add domain to platform API unless env vars are present. Perhaps this should check we're in dev instead?

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -4,8 +4,13 @@ class Domain
   end
 
   def save
-    heroku = PlatformAPI.connect_oauth ENV['PLATFORM_API_TOKEN']
-    heroku.domain.create(ENV['PLATFORM_API_APP_NAM'], { hostname: @domain })
+    token = ENV['PLATFORM_API_TOKEN']
+    app_name = ENV['PLATFORM_API_APP_NAME']
+
+    return unless token && app_name
+
+    heroku = PlatformAPI.connect_oauth token
+    heroku.domain.create app_name, { hostname: @domain }
   rescue Excon::Errors::UnprocessableEntity => e
     error = JSON.parse e.response.body
     unless error['message'] == 'Domain already added to this app.'
